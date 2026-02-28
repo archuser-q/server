2e981b376c5c76c0f8c92c58e56925fc02934735
archuser-q <kingdomofhellborn123@gmail.com>
2026-02-28 23:27:48 +0700

    adding login count

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -21,3 +21,41 @@ exports.getLoginHistory = async (request, reply) => {
   const list = await LoginHistory.find(query).sort({ timestamp: -1 });
   return list;
 };
+
+exports.getLoginCount = async (request, reply) => {
+  try {
+    const { username } = request.query;
+    const sevenDaysAgo = new Date();
+    sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+
+    const pipeline = [
+      {
+        $match: {
+          timestamp: { $gte: sevenDaysAgo },
+          ...(username ? { username: username.trim() } : {})
+        }
+      },
+      {
+        $group: {
+          _id: { $dateToString: { format: "%Y-%m-%d", date: "$timestamp" } },
+          count: { $sum: 1 }
+        }
+      },
+      { $sort: { "_id": 1 } },
+      {
+        $project: {
+          _id: 0,
+          timestamp: "$_id",
+          count:1
+        }
+      }
+    ];
+    const stats = await LoginHistory.aggregate(pipeline);
+    return {
+      success: true,
+      data: stats
+    };
+  } catch (err) {
+    reply.code(500).send({ error: err.message });
+  }
+};
--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -3,6 +3,7 @@ const loginController = require('../controllers/loginController');
 async function loginRoutes(fastify, options) {
   fastify.post('/login-history', loginController.createLoginEntry);
   fastify.get('/login-history', loginController.getLoginHistory);
+  fastify.get('/login-history/count', loginController.getLoginCount);
 }
 
 module.exports = loginRoutes;
